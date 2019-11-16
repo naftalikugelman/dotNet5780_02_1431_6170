18b03c188cc64e1d1867a63b00620c954b2343be
naftali kugelman <naftul6@gmail.com>
2019-11-16 21:19:18 +0200

    Add project files.

---
+++ dotNet5780_02_1431_6170/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_02_1431_6170
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
